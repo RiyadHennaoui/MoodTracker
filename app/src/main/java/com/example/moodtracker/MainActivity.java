2d4efef9828ea358eb0b4ae7d1aab9ae87f14c67
RiyadHennaoui <riyad.hennaoui@gmail.com>
2019-03-24 06:21:43 +0100

    Enum Mood created with method to recover color and drawable Mood

--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.moodtracker;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout backgraoundMoods;
+    ImageView faceMoods;
+    ImageView noteAdd;
+    ImageView history;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        backgraoundMoods = findViewById(R.id.background_moods);
+        faceMoods = findViewById(R.id.face_moods);
+        noteAdd = findViewById(R.id.note_add);
+        history = findViewById(R.id.history);
+
+
     }
 }
---
+++ app/src/main/java/com/example/moodtracker/Mood.java
@@ -0,0 +1,33 @@
+package com.example.moodtracker;
+
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+
+public enum Mood {
+
+    SUPER_HAPPY(R.color.banana_yellow, R.drawable.smiley_super_happy),
+    HAPPY(R.color.light_sage,R.drawable.smiley_happy),
+    NORMAL(R.color.cornflower_blue_65,R.drawable.smiley_normal),
+    SAD(R.color.warm_grey,R.drawable.smiley_sad),
+    DISAPPOINTED(R.color.faded_red,R.drawable.smiley_disappointed);
+
+    @ColorRes
+    int colorRes;
+    @DrawableRes
+    int drawableRes;
+
+    public int getColorRes() {
+        return colorRes;
+    }
+
+    public int getDrawableRes() {
+        return drawableRes;
+    }
+
+    Mood(@ColorRes int  colorRes,@DrawableRes int drawableRes) {
+
+        this.colorRes = colorRes;
+        this.drawableRes = drawableRes;
+
+    }
+}
