036d34a641a1356add400a3cf6e70074fd4034f8
RiyadHennaoui <riyad.hennaoui@gmail.com>
2019-04-18 08:19:54 +0200

    MainApplication class created to init new dependencies. Realm and ABP 310 init added. upgrade the Onfling method. save

--- app/src/main/java/com/example/moodtracker/DailyMood.java
+++ app/src/main/java/com/example/moodtracker/DailyMood.java
@@ -1,11 +1,17 @@
 package com.example.moodtracker;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
 
 public class DailyMood extends RealmObject {
 
     private String comment;
     private String mood;
+    @PrimaryKey
+    @Required
+    private String date;
+
 
 
 
@@ -23,5 +29,13 @@ public class DailyMood extends RealmObject {
 
     public void saveMood(Mood mood) {
         this.mood = mood.name();
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.moodtracker;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.threeten.bp.LocalDate;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
+import io.realm.Sort;
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
@@ -38,23 +48,36 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         faceMoods = findViewById(R.id.face_moods);
         noteAdd = findViewById(R.id.note_add);
         history = findViewById(R.id.history);
+
+        Log.i("date", LocalDate.now().toString());
         Realm.getDefaultInstance().beginTransaction();
-        DailyMood todayMood =  Realm.getDefaultInstance().createObject(DailyMood.class);
-        todayMood.setComment("test");
-        todayMood.saveMood(Mood.HAPPY);
+        DailyMood dailyMood = new DailyMood();
+        dailyMood.setComment("test1");
+        dailyMood.setDate(LocalDate.now().toString());
+        dailyMood.saveMood(Mood.HAPPY);
+        DailyMood todayMood = Realm.getDefaultInstance().createObject(DailyMood.class, dailyMood);
         Realm.getDefaultInstance().commitTransaction();
 
-        RealmResults<DailyMood> results = Realm.getDefaultInstance().where(DailyMood.class).findAll();
-        Toast.makeText(this,results.get(0).getComment(),Toast.LENGTH_LONG).show();
+
+        RealmResults<DailyMood> results = Realm.getDefaultInstance()
+                .where(DailyMood.class)
+                .sort("date", Sort.DESCENDING)
+                .findAll();
+        Toast.makeText(this, results.get(0).getComment(), Toast.LENGTH_LONG).show();
+
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent moodHistory = new Intent(MainActivity.this, MoodHistory.class);
+                startActivity(moodHistory);
+
+            }
+        });
 
     }
 
-
-    public void userComments(View v) {
-
-        DialogComments.showCommentsDiags(this);
-
-    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -92,23 +115,59 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
         boolean shouldConsumeEvent = false;
 
-        if (velocityY < 0 ) {
+        if (velocityY < 0) {
             shouldConsumeEvent = true;
             i++;
 
-        }else if ( velocityY > 0 ) {
+        } else if (velocityY > 0) {
             shouldConsumeEvent = true;
             i--;
 
         }
+        if (i < 0) {
 
+            i = 0;
+
+        } else if (i > Mood.values().length - 1) {
+
+            i = Mood.values().length - 1;
+
+        }
 
 
         backgraoundMoods.setBackgroundResource(Mood.values()[i].getColorRes());
         faceMoods.setImageResource(Mood.values()[i].getDrawableRes());
 
+
+
         return shouldConsumeEvent;
 
 
     }
+
+
+//    private void creatAndDisplayDialog(){
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        LinearLayout layout = new LinearLayout(this);
+//        TextView message = new TextView(this);
+//        final EditText inputComment = new EditText(this);
+//
+//        message.setText("Commentaire");
+//        message.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f);
+//        inputComment.setSingleLine();
+//        layout.setOrientation(LinearLayout.VERTICAL);
+//        layout.addView(message);
+//        layout.addView(inputComment);
+//        layout.setPadding(50,40,50,10);
+//
+//        builder.setView(layout);
+//
+//        builder.setNegativeButton("ANNULER", DialogInterface.OnClickListener auditeur)
+//
+//
+//
+//    }
+
+
 }
